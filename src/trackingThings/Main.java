bf5344e4913e8137e28373b2a88df57f1e5411f8
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2017-08-01 09:03:43 -0300

    ll

--- src/trackingThings/Main.java
+++ src/trackingThings/Main.java
@@ -1,14 +1,12 @@
 package trackingThings;
 
+import easyaccept.EasyAccept;
+import easyaccept.EasyAcceptException;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("vinicinho");
-		System.out.println("arizin");
-		System.out.println("Vinicin");
-		System.out.println("Vinicsssssin");
-		System.out.println("Vinicin");
-		System.out.println("Vinicin");
-		System.out.println("Vinicin");
+		
+		System.out.println("Kkkkk");
+		
 	}
 }
