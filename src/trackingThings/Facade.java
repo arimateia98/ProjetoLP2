b7da436777f005ecf117297536a8841affefce99
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2017-08-01 09:40:07 -0300

    e ese jeito aqui

---
+++ src/trackingThings/Facade.java
@@ -0,0 +1,14 @@
+package trackingThings;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import easyaccept.EasyAccept;
+
+public class Facade {
+
+	public static void main(String[] args) { 
+		args = new String[] {"trackingThings.Facade", "us/us1_test.txt"};
+		EasyAccept.main(args);
+	}
+}
--- src/trackingThings/Main.java
+++ src/trackingThings/Main.java
@@ -4,17 +4,8 @@ import java.util.ArrayList;
 
 import java.io.File;
 import easyaccept.EasyAccept;
-import easyaccept.EasyAcceptException;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		
-		ArrayList<String> testes = new ArrayList<>();
-		testes.add("us" + File.separator + "us1_test.txt");
-		testes.add("us" + File.separator + "us2_test.txt");
-		testes.add("us" + File.separator + "us3_test.txt");
-		testes.add("us" + File.separator + "us4_test.txt");
-		EasyAccept.executeEasyAcceptTests("trackingThings.Facade", testes);
-	}
+
 }
