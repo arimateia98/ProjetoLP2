61002cd36c458b5642a93cffb7d668cdd49c96dc
Jose Vinicius Lacerda de Arruda 116210766 <josevla@lcc2-06.lcc.ufcg.edu.br>
2017-08-01 13:43:46 -0300

    Soh fazendo o corpo

--- src/trackingThings/Facade.java
+++ src/trackingThings/Facade.java
@@ -1,14 +1,22 @@
 package trackingThings;
 
-import java.io.File;
-import java.util.ArrayList;
-
-import easyaccept.EasyAccept;
 
 public class Facade {
-
-	public static void main(String[] args) { 
-		args = new String[] {"trackingThings.Facade", "us/us1_test.txt"};
-		EasyAccept.main(args);
+	
+	public Facade(){
+		
 	}
+	
+	public void cadastrarUsuario(String nome,String telefone, String email){
+		
+	}
+	
+	public void removerUsuario(String nome, String telefone){
+		
+	}
+	
+	public void atualizarUsuario(String nome,String telefone, String atributo, String email){
+		
+	}
+
 }
