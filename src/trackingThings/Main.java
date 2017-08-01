de5b81eb60288c134b27a566f547d5cf9b23d64b
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2017-08-01 09:23:12 -0300

    kkK

--- src/trackingThings/Main.java
+++ src/trackingThings/Main.java
@@ -1,23 +1,12 @@
 package trackingThings;
 
-<<<<<<< HEAD
-import java.util.ArrayList;
-
-=======
-import java.io.File;
-import java.util.ArrayList;
->>>>>>> cf6a0b1b9538197c5b69ec106d0db170d98fd6e6
 import easyaccept.EasyAccept;
-
-
+import easyaccept.EasyAcceptException;
 public class Main {
 	
 	public static void main(String[] args) {
-		ArrayList<String> testes = new ArrayList<>();
-		testes.add("us", "us1_test.txt");
-		testes.add("us", "us2_test.txt");
-		testes.add("us", "us3_test.txt");
-		testes.add("us", "us4_test.txt");
-		EasyAccept.executeEasyAcceptTests("trackingThings.Facade", testes);
+		
+		System.out.println("kkkk");
+		
 	}
 }
