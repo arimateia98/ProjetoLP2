5e8351a52554196ba83145cf349a772e4ec5e9dd
José Vinícius Lacerda de Arruda <viniciuslacerda02@hotmail.com>
2017-08-01 09:05:26 -0300

    Update Main.java
    mudança

--- src/trackingThings/Main.java
+++ src/trackingThings/Main.java
@@ -5,8 +5,11 @@ import easyaccept.EasyAcceptException;
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		System.out.println("Kkkkk");
-		
+		ArrayList<String> testes = new ArrayList<>();
+		testes.add("us" + File.separator + "us1_test.txt");
+		testes.add("us" + File.separator + "us2_test.txt");
+		testes.add("us" + File.separator + "us3_test.txt");
+		testes.add("us" + File.separator + "us4_test.txt");
+		EasyAccept.executeEasyAcceptTests("trackingThings.Facade", testes);
 	}
 }
