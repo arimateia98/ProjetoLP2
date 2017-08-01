3fbec22febe6a6ae6d13da32a3e6940718cc6a65
viniciuslacerda0 <viniciuslacerda02@hotmail.com>
2017-08-01 09:24:42 -0300

    mudei o main

--- src/trackingThings/Main.java
+++ src/trackingThings/Main.java
@@ -1,12 +1,20 @@
 package trackingThings;
 
+import java.util.ArrayList;
+
+import java.io.File;
 import easyaccept.EasyAccept;
 import easyaccept.EasyAcceptException;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("kkkk");
-		
+		ArrayList<String> testes = new ArrayList<>();
+		testes.add("us" + File.separator + "us1_test.txt");
+		testes.add("us" + File.separator + "us2_test.txt");
+		testes.add("us" + File.separator + "us3_test.txt");
+		testes.add("us" + File.separator + "us4_test.txt");
+		EasyAccept.executeEasyAcceptTests("trackingThings.Facade", testes);
 	}
 }
