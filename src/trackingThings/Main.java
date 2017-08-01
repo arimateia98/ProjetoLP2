f02baab9da0d226cc0c3a8dba782e8b8ecd9d7b8
ramonfragoso <jose.ramon.silva@ccc.ufcg.edu.br>
2017-08-01 09:17:34 -0300

    kk

--- src/trackingThings/Main.java
+++ src/trackingThings/Main.java
@@ -1,15 +1,17 @@
 package trackingThings;
 
+import java.util.ArrayList;
+
 import easyaccept.EasyAccept;
 import easyaccept.EasyAcceptException;
 public class Main {
 	
 	public static void main(String[] args) {
 		ArrayList<String> testes = new ArrayList<>();
-		testes.add("us" + File.separator + "us1_test.txt");
-		testes.add("us" + File.separator + "us2_test.txt");
-		testes.add("us" + File.separator + "us3_test.txt");
-		testes.add("us" + File.separator + "us4_test.txt");
+		testes.add("us", "us1_test.txt");
+		testes.add("us", "us2_test.txt");
+		testes.add("us", "us3_test.txt");
+		testes.add("us", "us4_test.txt");
 		EasyAccept.executeEasyAcceptTests("trackingThings.Facade", testes);
 	}
 }
